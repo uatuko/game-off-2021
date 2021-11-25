21c0bc10ae3d9f0acba3caa1bf91bac33ca91360
Laura Necula <neculalaura@gmail.com>
2021-11-25 22:07:56 +0000

    issue #14 - mushroom maze part 3

--- src/levels/MushroomsMaze.gd
+++ src/levels/MushroomsMaze.gd
@@ -3,3 +3,8 @@ extends Node2D
 func _ready():
 	$Player/Sprite.flip_h = true
 	$Player._can_double_jump = false 
+
+
+func _on_DoubleJumpPowerUp_body_entered(body):
+	$Player._can_double_jump = true 
+	print("can double jump")
