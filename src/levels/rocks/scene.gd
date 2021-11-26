f8f2046d803fe08f76354de760a726c290b69bee
Uditha Atukorala <ua@nuked.zone>
2021-11-26 18:05:13 +0000

    (levels/rocks) #18 - reset level when hurt

--- src/levels/rocks/scene.gd
+++ src/levels/rocks/scene.gd
@@ -3,9 +3,5 @@ extends Node2D
 
 func _on_HurtBoxes_body_entered(body):
 	if body is Player:
-		# TODO:
-		#   - Player die animation
-		#   - End current run
-		#   - Reset to beginning
-
-		print("hit")
+		yield(body.die(), "completed")
+		get_tree().reload_current_scene()
