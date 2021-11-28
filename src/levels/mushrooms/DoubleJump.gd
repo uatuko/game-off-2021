d44c88478ca56d496244ec6a445ef9f2c96425eb
Laura Necula <neculalaura@gmail.com>
2021-11-28 17:17:09 +0000

    issue #14 - mushroom level v2.6 - power up animation

--- src/levels/mushrooms/DoubleJump.gd
+++ src/levels/mushrooms/DoubleJump.gd
@@ -5,5 +5,8 @@ extends Area2D
 func _on_DoubleJump_body_entered(body):
 	if body is Player:
 		body._can_double_jump = true
-		
+		$AnimationPlayer.play("bounce")
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
 	queue_free()
